50d3a7da05b26be54fc4d7bb715497ad3f83683f
wx1458084829 <wx1999125>
2021-12-14 13:45:01 +0800

    更正变量名

--- src/main/java/com/namo/singleton/Singleton1.java
+++ src/main/java/com/namo/singleton/Singleton1.java
@@ -5,15 +5,15 @@ package com.namo.singleton;
  */
 public class Singleton1 {
     //对象实例
-    private static Singleton1 instance;
+    private static Singleton1 instance1;
     //私有化构造方法
     private Singleton1() {
     }
     //创建对象
     public static Singleton1 getUniqueInstance() {
-        if (instance == null) {
-            instance = new Singleton1();
+        if (instance1 == null) {
+            instance1 = new Singleton1();
         }
-        return instance;
+        return instance1;
     }
 }
