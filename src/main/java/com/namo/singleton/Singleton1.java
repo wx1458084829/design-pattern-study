5da358331d036a8a9f09a52d45debf5765a706bb
wx1458084829 <wx1999125>
2021-12-14 13:47:31 +0800

    增加测试

--- src/main/java/com/namo/singleton/Singleton1.java
+++ src/main/java/com/namo/singleton/Singleton1.java
@@ -10,10 +10,17 @@ public class Singleton1 {
     private Singleton1() {
     }
     //创建对象
-    public static Singleton1 getUniqueInstance() {
+    public static Singleton1 getInstance() {
         if (instance1 == null) {
             instance1 = new Singleton1();
         }
         return instance1;
+    }
+
+    public static void main(String[] args) {
+        Singleton1 singleton1 = Singleton1.getInstance();
+        Singleton1 singleton2 = Singleton1.getInstance();
+        System.out.println("singleton1.hashCode() = " + singleton1.hashCode());
+        System.out.println("singleton2.hashCode() = " + singleton2.hashCode());
     }
 }
