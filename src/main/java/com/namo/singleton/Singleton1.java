c1a78ada37a92d6ae8e5e33540497e8d733ed263
wx1458084829 <wx1999125>
2021-12-14 13:43:03 +0800

    更名

--- src/main/java/com/namo/singleton/Singleton1.java
+++ src/main/java/com/namo/singleton/Singleton1.java
@@ -3,16 +3,16 @@ package com.namo.singleton;
 /**
  * 懒汉-线程不安全
  */
-public class Singleton {
+public class Singleton1 {
     //对象实例
-    private static Singleton instance;
+    private static Singleton1 instance;
     //私有化构造方法
-    private Singleton() {
+    private Singleton1() {
     }
     //创建对象
-    public static Singleton getUniqueInstance() {
+    public static Singleton1 getUniqueInstance() {
         if (instance == null) {
-            instance = new Singleton();
+            instance = new Singleton1();
         }
         return instance;
     }
